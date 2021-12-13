2dda8f4265ea00f134d5bcc3ad9b027425bc6933
LouisMaupas <louismaupas@gmail.com>
2021-12-13 11:01:08 +0100

    WIP add logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import '../Styles/components/_header.scss'
+
+const logo = require('../public/logo.png')
 
 export default class Header extends Component {
     render() {
         return (
-            <div>
+            <div className='header'>
+                <img src={logo} alt='Logo' />
                 <nav>
                     <Link to="/">Accueil</Link>
                     <Link to="/about">A propos</Link>
