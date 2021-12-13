c7e78a45b9047174821b6b866fb94d9452ee4246
LouisMaupas <louismaupas@gmail.com>
2021-12-13 11:23:52 +0100

    font added

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './App.scss'
+import '../src/Styles/base/_typography.scss'
 
 ReactDOM.render(  
     <App />,
