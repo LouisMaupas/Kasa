b6d0dba9717e856d1492e43332990aeba1ed8a3e
lmaupas <louis.maupas@framatome.com>
2021-12-24 12:24:45 +0100

    adding about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import Dropdown from '../../components/Dropdown'
+//style sheet
+import about from '../../Styles/pages/_about.scss'
+//img
+import banner from '../../img/about_banner.png';
+
+
 
 export default class About extends Component {
     render() {
         return (
-            <div>
-                <div className='about__dropdowns'>
+            <div className='text-center'>
+                <img className='banner__img' src={banner} alt='banner' />
+                <div className='about__dropdowns d-flex flex-column align-items-center'>
                     <Dropdown title='Fiabilité'></Dropdown>
                     <Dropdown title='Respect'></Dropdown>
                     <Dropdown title='Service'></Dropdown>
