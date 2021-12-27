04d89205b42f0b35fffbbdc8a6112def0a0510b6
lmaupas <louis.maupas@framatome.com>
2021-12-27 11:57:06 +0100

    WIP carrousel

--- src/pages/Lodging/index.jsx
+++ src/pages/Lodging/index.jsx
@@ -5,7 +5,9 @@ import Tag from '../../components/Tag'
 // style sheet
 import lodging from '../../Styles/pages/_lodging.scss'
 //img
-import carrousel from '../../img/carrousel_one.png';
+import carrouselOne from '../../img/carrousel_one.png';
+import carrouselTwo from '../../img/carrousel_two.png';
+import carrouselThree from '../../img/carrousel_three.png';
 
 export default class Error extends Component {
     constructor(props) {
@@ -37,9 +39,17 @@ export default class Error extends Component {
           }
         }
         return (
-          <div className='wrapper'>
-            <div className='text-center'>
-              <img className='banner__img' src={carrousel} alt='banner' />
+          <div className='wrapper d-flex flex-column'>
+
+
+            <div className='carrousel'>
+              <div className='slides'>
+                <div className='carrousel-left'> G </div>
+                <div className='carrousel-right'> D </div>
+                <div className="slide"><img src={carrouselOne} alt='banner' /></div>
+                <div className="slide"><img src={carrouselTwo} alt='banner' /></div>
+                <div className="slide"><img src={carrouselThree} alt='banner' /></div>
+              </div>
             </div>
 
             <div className='d-flex'>
