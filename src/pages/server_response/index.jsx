051d2e11a2a36db4d88510d8551df3cf40c4676d
lmaupas <louis.maupas@framatome.com>
2021-12-29 17:25:38 +0100

    ui

--- src/pages/server_response/index.jsx
+++ src/pages/server_response/index.jsx
@@ -6,10 +6,10 @@ import server_response from '../../Styles/pages/_server_response.scss'
 export default class NotFound extends Component {
     render() {
         return (
-            <div className='d-flex flex-column text-center'>
+            <div className='d-flex flex-column text-center server_response_page'>
                 <span className='http-code'>404</span>
                 <span className='server-response'>Oups! La page que vous demandez n'existe pas.</span>
-                <NavLink className='return-home' to="/" exact>
+                <NavLink className='return-home mt-5' to="/" exact>
                     Retourner sur la page d'accueil
                 </NavLink >
             </div>
