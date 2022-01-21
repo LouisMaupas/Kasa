87ace0799c252fa958710bc89fe0e63d11ffa534
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2022-01-21 16:36:19 +0100

    ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/Home'
-import Header from './components/Header'
+import Header from './components/header'
 import Footer from './components/Footer'
 import server_response from './pages/server_response'
 import About from './pages/About'
